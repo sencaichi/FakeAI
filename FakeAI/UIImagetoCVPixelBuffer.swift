b546115175716d65216dcaf26ed10461828ce5a5
Sen <sencaichi@gmail.com>
2024-10-22 22:08:34 -0400

    UIImagetoCVPixelBuffer

---
+++ FakeAI/UIImagetoCVPixelBuffer.swift
@@ -0,0 +1,40 @@
+//
+//  UIImagetoCVPixelBuffer.swift
+//  FakeAI
+//
+//  Created by Sen Cai on 10/22/24.
+//
+import UIKit
+
+extension UIImage {
+    func toCVPixelBuffer() -> CVPixelBuffer? {
+        let width = Int(self.size.width)
+        let height = Int(self.size.height)
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue,
+             kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer: CVPixelBuffer?
+        CVPixelBufferCreate(kCFAllocatorDefault, width, height, kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        
+        guard let buffer = pixelBuffer else {
+            return nil
+        }
+        
+        CVPixelBufferLockBaseAddress(buffer, .readOnly)
+        let context = CGContext(data: CVPixelBufferGetBaseAddress(buffer),
+                                width: width,
+                                height: height,
+                                bitsPerComponent: 8,
+                                bytesPerRow: CVPixelBufferGetBytesPerRow(buffer),
+                                space: CGColorSpaceCreateDeviceRGB(),
+                                bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
+        
+        guard let cgImage = self.cgImage else {
+            return nil
+        }
+        
+        context?.draw(cgImage, in: CGRect(x: 0, y: 0, width: width, height: height))
+        CVPixelBufferUnlockBaseAddress(buffer, .readOnly)
+        
+        return buffer
+    }
+}
