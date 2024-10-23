66a23ac7f847ce2a03e35c091744bf742f443e0b
Sen <sencaichi@gmail.com>
2024-10-23 05:30:35 -0400

    CGImagePropertyOrientation+UIImageOrientation

---
+++ FakeAI/CGImagePropertyOrientation+UIImageOrientation.swift
@@ -0,0 +1,28 @@
+//
+//  CGImagePropertyOrientation+UIImageOrientation.swift
+//  FakeAI
+//
+//  Created by Sen Cai on 10/23/24.
+//
+import UIKit
+import ImageIO
+
+extension CGImagePropertyOrientation {
+    /// Converts an image orientation to a Core Graphics image property orientation.
+    /// - Parameter orientation: A `UIImage.Orientation` instance.
+    ///
+    /// The two orientation types use different raw values.
+    init(_ orientation: UIImage.Orientation) {
+        switch orientation {
+            case .up: self = .up
+            case .down: self = .down
+            case .left: self = .left
+            case .right: self = .right
+            case .upMirrored: self = .upMirrored
+            case .downMirrored: self = .downMirrored
+            case .leftMirrored: self = .leftMirrored
+            case .rightMirrored: self = .rightMirrored
+            @unknown default: self = .up
+        }
+    }
+}
